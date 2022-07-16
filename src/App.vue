ed4b2db5961deb764f4f59e5e8cd5fa772409dd1
datanh98hp <dat198hp@gmail.com>
2022-07-16 13:55:45 +0700

    commit160722

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <!-- <Login/> -->
+<Layout/>
+</template>
+
+<script>
+//  
+// import Login from "./components/Login.vue"
+import Layout from './layout/index.vue'
+export default {
+  name: 'App',
+  components: {
+    // Login,
+    Layout
+
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+}
+</style>
